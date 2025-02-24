d1e286440c406644c203fce5042bcb535ea15090
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-02-24 19:10:47 -0300

    Ajustando o Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
@@ -35,8 +37,11 @@ const Header = () => {
       </div>
 
       {/* Header com Nav */}
-      <div className="bg-primary text-white">
-        <div className="container mx-auto flex justify-between items-center">
+      <div className="bg-primary text-white px-2">
+        <div
+          className="container mx-auto flex justify-center
+         items-center"
+        >
           {/* Logo */}
           <div className="text-2xl font-bold">
             <Link href="/">
@@ -45,30 +50,36 @@ const Header = () => {
                 alt="Logo"
                 width={350}
                 height={100}
-                className="mr-2"
+                className="mr-2 transform scale-125"
               />
             </Link>
           </div>
 
           {/* Navegação */}
-          <nav>
-            <ul className="flex space-x-8 text-lg uppercase font-bold">
-              {[
-                { name: 'Home', path: '/' },
-                { name: 'A Liga', path: '/liga' },
-                { name: 'Clubes', path: '/clubes' },
-                { name: 'Competições', path: '/campeonato' },
-                { name: 'Notícias', path: '/notícias' }
-              ].map((item) => (
-                <li key={item.path} className="relative group">
-                  <Link href={item.path} className="relative p-2">
-                    {item.name}
-                    <span className="absolute left-1/2 -bottom-1 w-0 h-0.5 bg-white transition-all duration-300 ease-out origin-center group-hover:w-full group-hover:left-0"></span>
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </nav>
+          <div className="flex flex-col items-center mt-10">
+            <nav>
+              <ul className="flex space-x-8 text-lg uppercase font-bold">
+                {[
+                  { name: 'Home', path: '/' },
+                  { name: 'A Liga', path: '/liga' },
+                  { name: 'TV Liga Jundiaí', path: '/tv' },
+                  { name: 'Clubes', path: '/clubes' },
+                  { name: 'Competições', path: '/campeonato' },
+                  { name: 'Notícias', path: '/notícias' }
+                ].map((item) => (
+                  <li key={item.path} className="relative group">
+                    <Link href={item.path} className="relative p-2">
+                      {item.name}
+                      <span className="absolute left-1/2 -bottom-1 w-0 h-0.5 bg-white transition-all duration-300 ease-out origin-center group-hover:w-full group-hover:left-0"></span>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+            <p className="text-3xl uppercase font-bold text-center mt-4">
+              Liga de Futebol Amador Jundiaiense
+            </p>
+          </div>
         </div>
       </div>
     </header>
