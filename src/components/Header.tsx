ab5438b90f75d1475a8930a6df6cc3c549524f98
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-02-24 22:09:50 -0300

    Ajustando Component Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,22 @@
 'use client'
-
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import { FaFacebook, FaInstagram } from 'react-icons/fa'
 import { MdOutlineEmail } from 'react-icons/md'
+import { HiMenu, HiX } from 'react-icons/hi' // Ícones para menu mobile
 
 const Header = () => {
+  const [menuAberto, setMenuAberto] = useState(false)
+
   return (
     <header>
       {/* Barra superior */}
-      <div className="bg-foreground text-white p-2 flex justify-end items-center pr-20 space-x-8">
+      <div className="hidden lg:flex bg-foreground text-white p-2 justify-end items-center pr-20 space-x-8">
         <div className="flex items-center space-x-1">
           <MdOutlineEmail size={25} />
           <Link href="/contact" className="hover:underline">
-            Fale Conozco
+            Fale Conosco
           </Link>
         </div>
 
@@ -37,26 +39,84 @@ const Header = () => {
       </div>
 
       {/* Header com Nav */}
-      <div className="bg-primary text-white px-2">
+      <div className="bg-primary text-white lg:py-4 py-2">
         <div
           className="container mx-auto flex justify-center
-         items-center"
+         items-center lg:py-4"
         >
           {/* Logo */}
-          <div className="text-2xl font-bold">
+          <div className="relative lg:absolute lg:right-1/2 lg:transform lg:translate-x-40 text-2xl font-bold w-full flex justify-center items-center">
             <Link href="/">
               <Image
                 src="/assets/logo_liga.png"
                 alt="Logo"
-                width={350}
-                height={100}
-                className="mr-2 transform scale-125"
+                width={300}
+                height={70}
+                className="scale-150"
               />
             </Link>
           </div>
 
-          {/* Navegação */}
-          <div className="flex flex-col items-center mt-10">
+          {/* Botão de Menu Hambúrguer */}
+          <button
+            className="flex items-center ml-auto text-white text-4xl lg:hidden p-5"
+            onClick={() => setMenuAberto(!menuAberto)}
+          >
+            {menuAberto ? <HiX /> : <HiMenu />}
+          </button>
+
+          {/* Menu Mobile (sem array) */}
+          {menuAberto && (
+            <div className="fixed top-0 left-0 w-full h-full bg-primary text-white flex flex-col items-center justify-center space-y-6">
+              {[
+                { name: 'Home', path: '/' },
+                { name: 'A Liga', path: '/liga' },
+                { name: 'TV Liga Jundiaí', path: '/tv' },
+                { name: 'Clubes', path: '/clubes' },
+                { name: 'Competições', path: '/campeonato' },
+                { name: 'Notícias', path: '/notícias' },
+                { name: 'Fale Conosco', path: '/contact' }
+              ].map((item, index) => (
+                <Link
+                  key={index}
+                  href={item.path}
+                  className="text-2xl uppercase font-bold"
+                  onClick={() => setMenuAberto(false)}
+                >
+                  {item.name}
+                </Link>
+              ))}
+
+              {/* Ícones das Redes Sociais */}
+              <div className="flex space-x-4 mt-4">
+                <Link
+                  href="https://www.facebook.com"
+                  target="_blank"
+                  className="hover:text-blue-600"
+                >
+                  <FaFacebook size={25} />
+                </Link>
+                <Link
+                  href="https://www.instagram.com"
+                  target="_blank"
+                  className="hover:text-pink-600"
+                >
+                  <FaInstagram size={25} />
+                </Link>
+              </div>
+
+              {/* Botão para Fechar o Menu */}
+              <button
+                className="text-white text-4xl absolute top-5 right-5"
+                onClick={() => setMenuAberto(false)}
+              >
+                <HiX />
+              </button>
+            </div>
+          )}
+
+          {/* Navegação Desktop */}
+          <div className="hidden lg:flex flex-col items-center mt-8 ml-40">
             <nav>
               <ul className="flex space-x-8 text-lg uppercase font-bold">
                 {[
