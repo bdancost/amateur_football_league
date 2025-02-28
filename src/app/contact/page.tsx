99451b10b7c5177d3c6c2aa98672b15da0a697d1
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-02-27 21:49:12 -0300

    Ajustando pagina fale conosco

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className="flex flex-col">
+      <h1 className=" flex justify-center items-center mt-8 text-4xl font-bold uppercase">
+        Fale Conosco
+      </h1>
+
+      <div className="border-b-4 border-yellow-500 my-6 w-4/5 mx-auto"></div>
+    </div>
+  )
+}
+
+export default Contact
