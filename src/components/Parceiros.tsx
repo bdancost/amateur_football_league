3897be68bd084dff141ffee3ee8310fd67c74750
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-03-09 21:15:41 -0300

    Deixando o component responsivo

--- src/components/Parceiros.tsx
+++ src/components/Parceiros.tsx
@@ -1,40 +1,42 @@
 import React from 'react'
-import Image from 'next/image' // Certifique-se de importar corretamente
+import Image from 'next/image'
 
 const Parceiros = () => {
   return (
     <>
-      {/* Container dos Parceiros */}
-      <div className="bg-gray-100 lg:w-3/5 w-full p-3 rounded-lg mt-10">
-        <h2 className="text-xl font-bold text-gray-800 mb-4 uppercase">
+      {/* Título */}
+      <div className="bg-gray-100 lg:w-3/5 w-full p-3 rounded-lg mt-10 mx-auto">
+        <h2 className="text-xl font-bold text-gray-800 mb-4 uppercase text-center">
           Parceiros da Liga 2025
         </h2>
       </div>
 
-      {/* Grid de imagens */}
-      <div className="lg:w-3/5 w-full grid grid-cols-3 justify-items-center mt-4">
+      {/* Grid Responsivo */}
+      <div className="lg:w-3/5 w-full grid grid-cols-3 md:grid-cols-3 sm:grid-cols-2 xs:grid-cols-1 justify-items-center mt-4 mx-auto gap-4">
         {['uniex', 'choperia', 'mb_news'].map((partner) => (
-          <Image
-            key={partner}
-            src={`/assets/${partner}.png`}
-            alt={`Logo ${partner.replace('_', ' ')}`}
-            width={300} // Aumento proporcional
-            height={300}
-            className="object-contain"
-          />
-        ))}
-
-        {/* Segunda linha - Forçando 2 colunas */}
-        <div className="col-span-3 flex justify-center items-center gap-6 flex-wrap">
-          {['tintino_pizza', 'drogaria'].map((partner) => (
+          <div key={partner} className="w-64 md:w-72 lg:w-80">
             <Image
-              key={partner}
               src={`/assets/${partner}.png`}
               alt={`Logo ${partner.replace('_', ' ')}`}
-              width={350}
-              height={350}
-              className="object-contain"
+              width={400}
+              height={400}
+              className="w-full h-auto object-contain"
             />
+          </div>
+        ))}
+
+        {/* Segunda linha */}
+        <div className="col-span-3 flex flex-wrap justify-center items-center gap-6 sm:gap-8 flex-col sm:flex-row">
+          {['tintino_pizza', 'drogaria'].map((partner) => (
+            <div key={partner} className="w-64 md:w-72 lg:w-80">
+              <Image
+                src={`/assets/${partner}.png`}
+                alt={`Logo ${partner.replace('_', ' ')}`}
+                width={400}
+                height={400}
+                className="w-full h-auto object-contain"
+              />
+            </div>
           ))}
         </div>
       </div>
