0fb656e6ae9ceacc6e540295f08a4884517f7a3d
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-03-17 21:05:10 -0300

    Configurando a responsividade do component Parceiros

--- src/components/Parceiros.tsx
+++ src/components/Parceiros.tsx
@@ -12,9 +12,25 @@ const Parceiros = () => {
       </div>
 
       {/* Grid Responsivo */}
-      <div className="lg:w-3/5 w-full grid grid-cols-2 md:grid-cols-3 justify-items-center mt-4 mx-auto gap-6">
-        {['uniex', 'choperia', 'mb_news', 'tintino_pizza', 'drogaria'].map(
-          (partner) => (
+      <div className="lg:w-3/5 w-full grid grid-cols-3 gap-6 justify-items-center mt-4 mx-auto">
+        {['uniex', 'choperia', 'mb_news'].map((partner) => (
+          <div
+            key={partner}
+            className="w-[180px] sm:w-[220px] md:w-[260px] lg:w-96"
+          >
+            <Image
+              src={`/assets/${partner}.png`}
+              alt={`Logo ${partner.replace('_', ' ')}`}
+              width={400}
+              height={400}
+              className="w-full h-auto object-contain"
+            />
+          </div>
+        ))}
+
+        {/* Container para centralizar os dois últimos elementos */}
+        <div className="col-span-3 flex justify-center gap-6 w-full">
+          {['tintino_pizza', 'drogaria'].map((partner) => (
             <div
               key={partner}
               className="w-[180px] sm:w-[220px] md:w-[260px] lg:w-96"
@@ -27,8 +43,8 @@ const Parceiros = () => {
                 className="w-full h-auto object-contain"
               />
             </div>
-          )
-        )}
+          ))}
+        </div>
       </div>
     </>
   )
