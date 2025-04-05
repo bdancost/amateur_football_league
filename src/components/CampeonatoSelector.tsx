1e900109861089af0884800d7da939e36b794fe8
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-04-05 07:36:11 -0300

    Configurando página competiçoes

--- src/components/CampeonatoSelector.tsx
+++ src/components/CampeonatoSelector.tsx
@@ -10,24 +10,22 @@ const CampeonatoSelector = ({
   setCampeonatoSelecionado
 }: CampeonatoSelectorProps) => {
   return (
-    <div>
+    <div className="flex flex-wrap gap-4 mb-4 ml-10 mr-10">
       {sections.map((campeonato) => (
         <button
           key={campeonato.id}
           onClick={() => setCampeonatoSelecionado(campeonato.id)}
-          style={{
-            margin: '5px',
-            padding: '10px',
-            backgroundColor:
-              campeonato.id === campeonatoSelecionado ? 'blue' : 'gray',
-            color: 'white',
-            border: 'none',
-            cursor: 'pointer'
-          }}
+          className={`px-4 py-2 rounded text-white font-semibold transition mb-10
+            ${
+              campeonato.id === campeonatoSelecionado
+                ? 'bg-foreground hover:bg-foreground/80'
+                : 'bg-gray-500 hover:bg-gray-600'
+            }`}
         >
           {campeonato.title}
         </button>
       ))}
+      <div className=" w-full border-b-2"></div>
     </div>
   )
 }
