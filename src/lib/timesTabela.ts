a2cbe81570e5948393f62eed88d863bfeb176342
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-04-06 11:13:22 -0300

    Ajustando pagina Competiçoes

--- src/lib/timesTabela.ts
+++ src/lib/timesTabela.ts
@@ -13,11 +13,26 @@ export type Time = {
   campeonato: string
 }
 
+export const timesSub20: Time[] = [
+  {
+    id: 1,
+    nome: 'AA São Gonçalo',
+    imagem: '/assets/clubes/AA São Gonçalo.jpeg',
+    J: 0,
+    V: 0,
+    E: 0,
+    D: 0,
+    GP: 0,
+    GC: 0,
+    campeonato: 'Sub 20'
+  }
+]
+
 export const times: Time[] = [
   {
     id: 1,
-    nome: 'AE Cecap',
-    imagem: '/assets/clubes/AE CEcap.jpeg',
+    nome: 'AA Parque Aeroporto',
+    imagem: '/assets/clubes/AA Parque Aeroporto.jpeg',
     J: 0,
     V: 0,
     E: 0,
@@ -28,6 +43,30 @@ export const times: Time[] = [
   },
   {
     id: 2,
+    nome: 'AA Parque São Luiz',
+    imagem: '/assets/clubes/AA Parque São Luiz.jpeg',
+    J: 0,
+    V: 0,
+    E: 0,
+    D: 0,
+    GP: 0,
+    GC: 0,
+    campeonato: '1° Divisão'
+  },
+  {
+    id: 3,
+    nome: 'AA Rodoviário Cidade Jardim',
+    imagem: '/assets/clubes/AA Rodoviário Cidade Jardim.jpeg',
+    J: 0,
+    V: 0,
+    E: 0,
+    D: 0,
+    GP: 0,
+    GC: 0,
+    campeonato: '1° Divisão'
+  },
+  {
+    id: 4,
     nome: 'AA São Gonçalo',
     imagem: '/assets/clubes/AA São Gonçalo.jpeg',
     J: 0,
@@ -39,7 +78,7 @@ export const times: Time[] = [
     campeonato: '1° Divisão'
   },
   {
-    id: 3,
+    id: 5,
     nome: 'AE Vila São Geraldo',
     imagem: '/assets/clubes/AE Vila São Geraldo.jpeg',
     J: 0,
@@ -51,9 +90,9 @@ export const times: Time[] = [
     campeonato: '1° Divisão'
   },
   {
-    id: 4,
-    nome: 'CA Boca Junior Jardim América',
-    imagem: '/assets/clubes/CA Boca Junior Jardim América.jpeg',
+    id: 6,
+    nome: 'C Atlético Cecap',
+    imagem: '/assets/clubes/C Atlético Cecap.jpeg',
     J: 0,
     V: 0,
     E: 0,
@@ -63,7 +102,7 @@ export const times: Time[] = [
     campeonato: '1° Divisão'
   },
   {
-    id: 5,
+    id: 7,
     nome: 'CA Juventus',
     imagem: '/assets/clubes/CA Juventus.jpeg',
     J: 0,
@@ -75,7 +114,55 @@ export const times: Time[] = [
     campeonato: '1° Divisão'
   },
   {
-    id: 6,
+    id: 8,
+    nome: 'EC Ipanema',
+    imagem: '/assets/clubes/EC Ipanema.jpeg',
+    J: 0,
+    V: 0,
+    E: 0,
+    D: 0,
+    GP: 0,
+    GC: 0,
+    campeonato: '1° Divisão'
+  },
+  {
+    id: 9,
+    nome: 'EC Mourisco',
+    imagem: '/assets/clubes/EC Mourisco.jpeg',
+    J: 0,
+    V: 0,
+    E: 0,
+    D: 0,
+    GP: 0,
+    GC: 0,
+    campeonato: '1° Divisão'
+  },
+  {
+    id: 10,
+    nome: 'Água Quente',
+    imagem: '/assets/clubes/EC Água Quente.jpeg',
+    J: 0,
+    V: 0,
+    E: 0,
+    D: 0,
+    GP: 0,
+    GC: 0,
+    campeonato: '1° Divisão'
+  },
+  {
+    id: 11,
+    nome: 'EC Vila São José',
+    imagem: '/assets/clubes/EC Vila São José.jpeg',
+    J: 0,
+    V: 0,
+    E: 0,
+    D: 0,
+    GP: 0,
+    GC: 0,
+    campeonato: '1° Divisão'
+  },
+  {
+    id: 12,
     nome: 'EC XV de Novembro',
     imagem: '/assets/clubes/EC XV de Novembro.jpeg',
     J: 0,
@@ -87,9 +174,9 @@ export const times: Time[] = [
     campeonato: '1° Divisão'
   },
   {
-    id: 7,
-    nome: 'Rodoviário FC',
-    imagem: '/assets/clubes/Rodoviário FC.jpeg',
+    id: 13,
+    nome: 'SE Parque Urupês',
+    imagem: '/assets/clubes/SE Parque Urupês.jpeg',
     J: 0,
     V: 0,
     E: 0,
@@ -99,7 +186,31 @@ export const times: Time[] = [
     campeonato: '1° Divisão'
   },
   {
-    id: 8,
+    id: 14,
+    nome: 'União Operária FC',
+    imagem: '/assets/clubes/União Operária FC.jpeg',
+    J: 0,
+    V: 0,
+    E: 0,
+    D: 0,
+    GP: 0,
+    GC: 0,
+    campeonato: '1° Divisão'
+  },
+  {
+    id: 15,
+    nome: 'Unidos Marlene Miranda FC',
+    imagem: '/assets/clubes/Unidos Marlene Miranda FC.jpeg',
+    J: 0,
+    V: 0,
+    E: 0,
+    D: 0,
+    GP: 0,
+    GC: 0,
+    campeonato: '1° Divisão'
+  },
+  {
+    id: 16,
     nome: 'Unidos São Gonçalo FC',
     imagem: '/assets/clubes/Unidos São Gonçalo FC.jpeg',
     J: 0,
